6431bf1a5e6348ee9e6add09c19ceed3a038339e
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2023-09-06 12:14:59 +0200

    Finished building MeasuringAchievementStandard class.

--- App/MeasuringAchievementStandard.java
+++ App/MeasuringAchievementStandard.java
@@ -1,4 +1,29 @@
 package App;
+
 public class MeasuringAchievementStandard {
-    
+    protected String title;
+    protected String dailyTrackingQuestionSentence;
+    protected String measureStandardSymbol;
+
+    public MeasuringAchievementStandard(String title, String dailyTrackingQuestionSentence, String measureStandardSymbol)
+    {
+        this.title = title;
+        this.dailyTrackingQuestionSentence = dailyTrackingQuestionSentence;
+        this.measureStandardSymbol = measureStandardSymbol;
+    }
+
+    public String getTitle()
+    {
+        return this.title;
+    } 
+
+    public String getDailyTrackingQuestionSentence()
+    {
+        return this.dailyTrackingQuestionSentence;
+    }
+
+    public String getMeasureStandardSymbol()
+    {
+        return this.measureStandardSymbol;
+    }
 }
