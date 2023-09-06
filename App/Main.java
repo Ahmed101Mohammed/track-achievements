fc70dda5462770e84449392df9d808c527579eb6
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2023-09-06 12:17:09 +0200

    Test complated building main structure classes. The classes that I test are: - DB_Model - Achievement - MeasuringAchievementStandard

--- App/Main.java
+++ App/Main.java
@@ -3,5 +3,10 @@ package App;
 public class Main {
     public static void main(String[] args) {
         DB_Model.prepareeDB_Tables();
+        MeasuringAchievementStandard newOne = new MeasuringAchievementStandard("Reading Books", "How much pages you readed to day? ", "page(s)");
+        Achievement newAch = new Achievement("Reading Books", "How much pages you readed to day? ", "page(s)", "2023/9/6", 2);
+        DB_Model.addNewMeasuringAchievementStandard(newOne);
+        DB_Model.addNewAchievement(newAch);
+        System.out.println(DB_Model.getAllMeasuringAchievemntsStandards());
     }
 }
