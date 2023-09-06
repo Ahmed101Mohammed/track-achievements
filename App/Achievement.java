00a7c1c4767e34858789162a3b058114d10bfd49
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2023-09-06 12:13:51 +0200

    Finished building Achivement class.

--- App/Achievement.java
+++ App/Achievement.java
@@ -1,4 +1,24 @@
 package App;
-public class Achievement {
-    
+
+public class Achievement extends MeasuringAchievementStandard
+{
+    private String date;
+    private int progressValue;
+
+    public Achievement(String title, String dailyTrackingQuestionSentence, String measureStandardSymbol ,String date, int progress)
+    {
+        super(title, dailyTrackingQuestionSentence, measureStandardSymbol);
+        this.date = date;
+        this.progressValue = progress;
+    }
+
+    public String getDate()
+    {
+        return this.date;
+    }
+
+    public int getProgressValue()
+    {
+        return this.progressValue;
+    }
 }
