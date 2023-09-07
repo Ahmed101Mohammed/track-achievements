0db45846058f724ba012de0c8439e70b4d0fda1d
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2023-09-07 12:25:55 +0200

    Testing the functionality of the new classes that built. - MeasuringAchievementStandardServices class. - TrakingDailyServeice class.

--- App/Main.java
+++ App/Main.java
@@ -3,10 +3,17 @@ package App;
 public class Main {
     public static void main(String[] args) {
         DB_Model.prepareeDB_Tables();
-        MeasuringAchievementStandard newOne = new MeasuringAchievementStandard("Reading Books", "How much pages you readed to day? ", "page(s)");
-        Achievement newAch = new Achievement("Reading Books", "How much pages you readed to day? ", "page(s)", "2023/9/6", 2);
-        DB_Model.addNewMeasuringAchievementStandard(newOne);
-        DB_Model.addNewAchievement(newAch);
-        System.out.println(DB_Model.getAllMeasuringAchievemntsStandards());
+        //MeasuringAchievementStandardServices.serviceGuid();
+        TrakingDailyServeice.trackingProccess();
+    }
+
+    public static void quit(String input)
+    {
+       
+        if(input.equals("quit"))
+        {
+            System.out.println("Yow will exist from the app...");
+            System.exit(0);
+        }
     }
 }
