83052d33ade3634f16b914da251207724f0d6ce6
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2023-09-10 11:33:18 +0200

    Finshed the main window and the relations between classes.

--- App/Main.java
+++ App/Main.java
@@ -1,10 +1,35 @@
-package App; 
+package App;
+
+import java.util.Scanner;
 
 public class Main {
+    private static Scanner mainAppScanner = new Scanner(System.in);
     public static void main(String[] args) {
         DB_Model.prepareeDB_Tables();
-        //MeasuringAchievementStandardServices.serviceGuid();
-        TrakingDailyServeice.trackingProccess();
+        while(true)
+        {
+            String choosenService = startWindow();
+            quit(choosenService);
+            
+            switch(choosenService)
+            {
+                case "1":
+                    MeasuringAchievementStandardServices.serviceGuid();
+                    break;
+                case "2":
+                    TrackingDailyServeice.trackingProccess();
+                    break;
+                case "3":
+                    AnalysesServiece.servicesGuid();
+                    break;
+                case "../":
+                    quit("quit");
+                    break;
+                default:
+                    System.out.println("Please add a number for exist service.");
+            }
+
+        }
     }
 
     public static void quit(String input)
@@ -15,5 +40,20 @@ public class Main {
             System.out.println("Yow will exist from the app...");
             System.exit(0);
         }
+    }
+
+    private static String startWindow()
+    {
+        System.out.println("\nHi, I'm tracked achievements app. here you can: record your standards achivements,\n"+
+                                "And track them daily. Also with me you can get analytics for all of your achievements.\n"+
+                                "In any step of the programm you can print '../' to go back, and 'quit' to exist from the app.\n");
+            System.out.println("====================================\n"+
+                                "The services that I offer for you:\n"+
+                                "1. Measuring Achievement Standard Servieces.\n"+
+                                "2. Tracking Daily Serice.\n"+
+                                "3. Analyses Services.\n");
+            System.out.print("Add the number of service(Note: if you don't know how to use the app start from 1->3): ");
+            String choosenService = mainAppScanner.next();
+            return choosenService;
     }
 }
